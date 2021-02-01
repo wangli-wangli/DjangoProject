bc83d2b6e5b8dc2afc3808a242db20e4eab9d368
wangli-wangli <2024410102@qq.com>
2021-02-02 00:35:01 +0800

    连接mysql数据库

--- guest/sign/tests.py
+++ guest/sign/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-
+from sign.models import Event,Guest
 # Create your tests here.
+class ModelTest(TestCase):
+    def setUp(self):
+        Event
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import  HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from sign.models import Event
 
 # Create your views here.
 def index(request):
@@ -28,4 +29,15 @@ def login_action(request):
 @login_required
 def event_manage(request):
     username=request.session.get('user',' ')
-    return render(request,"event_manage.html",{"user":username})
+    event_list=Event.objects.all()
+    username=request.session.get('user','')
+    return render(request,"event_manage.html",{"user":username,"events":event_list})
+
+# 发布会名称搜索
+@login_required
+def search_name(request):
+    username = request.session.get('username', '')
+    search_name = request.GET.get("name", "")
+    search_name_bytes = search_name.encode(encoding="utf-8")
+    event_list = Event.objects.filter(name__contains=search_name_bytes)
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
