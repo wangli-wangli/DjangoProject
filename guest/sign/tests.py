1494f32675210ba854b26e21e653d1b9309bd8bb
wangli-wangli <2024410102@qq.com>
2021-02-19 00:39:55 +0800

    单元测试

--- guest/sign/tests.py
+++ guest/sign/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from sign.models import Event,Guest
+from django.contrib.auth.models import User
 # Create your tests here.
 class ModelTest(TestCase):
     def setUp(self):
@@ -15,4 +16,46 @@ class ModelTest(TestCase):
         result=Guest.objects.get(phone='12345678901')
         self.assertEqual(result.realname,"alen")
         self.assertFalse(result.sign)
+
+
+class IndexPageTest(TestCase):
+    '''测试index登录首页'''
+
+    def test_index_page_renders_index_template(self):
+        '''测试index视图'''
+        response=self.client.get('/index/')
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'index.html')
+
+class LoginActionTest(TestCase):
+    '''测试登录动作'''
+    def setUp(self):
+        User.objects.create_user('admin','admin@mail.com','admin123456')
+
+    def test_add_admin(self):
+        '''测试添加用户'''
+        user=User.objects.get(username="admin")
+        self.assertEqual(user.username,"admin")
+        self.assertEqual(user.email,'admin@mail.com')
+
+    def test_login_action_username_password_null(self):
+        '''用户名密码为空'''
+        test_data={'username':'','password':''}
+        response=self.client.post('/login_action/',data=test_data)
+        self.assertEqual(response.status_code,200)
+        self.assertIn(b"username or password error!",response.content)
+
+    def test_login_action_uername_password_error(self):
+        '''用户名密码错误'''
+        test_data={'username':'abc','password':'123'}
+        response=self.client.post('/login_action/',data=test_data)
+        self.assertEqual(response.status_code,200)
+        self.assertIn(b'username or password error!',response.content)
+
+    def test_login_action_success(self):
+        '''登录成功'''
+        test_data={'username':'admin','password':'admin123456'}
+        response=self.client.post('/login_action/',data=test_data)
+        self.assertEqual(response.status_code,302)
+
 
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -14,6 +14,7 @@ def login_action(request):
     if request.method=="POST":
         username=request.POST.get('username','')
         password=request.POST.get('password','')
+        print(username,"   ",password)
         user=auth.authenticate(username=username,password=password)
         if user is not None:
             auth.login(request,user)#登录
